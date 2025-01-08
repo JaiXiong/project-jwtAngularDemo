83fafedaa575c6968c29224dfc984adf1aaf5298
JaiX <xiongjai@gmail.com>
2025-01-08 14:53:46 -0600

    show info on home screen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: 
+  [
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+currentUser: any;
+authService = inject(AuthService);
 
+constructor() {
+  this.authService.getCurrentUser().subscribe(response => {
+    this.currentUser = response;
+  }
+  );
+}
 }
